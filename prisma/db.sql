405ebcccb6a3521b01efd4e71eb86b553f430682
Ashik Chapagain <projectashik@gmail.com>
2022-02-28 23:05:06 +0545

    fixed db.sql

--- prisma/db.sql
+++ prisma/db.sql
@@ -1,6 +1,23 @@
+create table account (
+  user_id serial primary key,
+  username varchar(255) unique not null,
+  password varchar(255) not null,
+  is_admin bool not null default false,
+  created_at timestamp with time zone default current_timestamp,
+  updated_at timestamp with time zone default current_timestamp
+);
+create table link (
+  link_id serial primary key,
+  user_id integer NOT NULL REFERENCES account(user_id) on delete cascade,
+  url text NOT NULL,
+  slug varchar(255) NOT NULL,
+  created_at timestamp(6) with time zone DEFAULT CURRENT_TIMESTAMP,
+  password varchar(60),
+  bot_protection bool not null default false
+);
 create table api (
   api_id serial primary key,
-  user_id integer not null references user(user_id) on delete cascade,
+  user_id integer not null references account(user_id) on delete cascade,
   key text NOT NULL,
   name text NOT NULL,
   created_at timestamp(6) with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
@@ -17,33 +34,15 @@ create table click (
   referral varchar(255),
   country varchar(255)
 );
-create table link (
-  link_id serial primary key,
-  user_id integer NOT NULL REFERENCES user(user_id) on delete cascade,
-  url text NOT NULL,
-  slug varchar(255) NOT NULL,
-  created_at timestamp(6) with time zone DEFAULT CURRENT_TIMESTAMP,
-  password varchar(60),
-  bot_protection bool not null default false
-);
-create table account (
-  user_id serial primary key,
-  username varchar(255) unique not null,
-  password varchar(60) not null,
-  is_admin bool not null default false,
-  created_at timestamp with time zone default current_timestamp,
-  updated_at timestamp with time zone default current_timestamp
-);
-create index api_user_id_idx on api using btree (user_id);
-create index click_link_id_idx on click using btree (link_id);
-create unique index link_slug_key on link using btree (slug);
-create index link_user_id_idx on link using btree (user_id);
-create unique index user_username_key on user using btree (username);
+create index api_user_id_idx on api(user_id);
+create index click_link_id_idx on click(link_id);
+create unique index link_slug_key on link(slug);
+create index link_user_id_idx on link(user_id);
 insert into
-  account(username, password, is_admin)
+  account (username, password, is_admin)
 values
   (
-    "admin",
-    "$2a$10$OEj1CERiX8FTEYCD60.12u.MwVM5OjZYy2Y3fUHCb6/H8BiSPvsoO",
+    'admin',
+    '$2b$10$BUli0c.muyCW1ErNJc3jL.vFRFtFJWrT8/GcR4A.sUdCznaXiqFXa',
     true
   );
